8f3996230671cb3a2a5b1d2bd0a82d8abb6caaa8
Artem Shvidchenko <0980211984artem@gmail.com>
2023-03-28 20:51:24 +0200

    Add enum

---
+++ src/Book.java
@@ -0,0 +1,13 @@
+public class Book {
+ public static void main(String[] args) {
+    enum Genres{
+      DRAMA,
+      FANTASY,
+      CLASSIC,
+      HISTORICAL,
+      DETECTIVE
+
+
+    }
+  }
+}
